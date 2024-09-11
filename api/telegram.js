cfe4abaa6cf2e9aa43d6f39c956baf26a3925386
snakeying <65364563+snakeying@users.noreply.github.com>
2024-09-12 01:22:17 +0800

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,10 +1,18 @@
 const bot = require('../src/bot');
 
 module.exports = async (req, res) => {
+  console.log('Received webhook request:', req.method, JSON.stringify(req.body));
   if (req.method === 'POST') {
-    await bot.processUpdate(req.body);
-    res.status(200).send('OK');
+    try {
+      await bot.processUpdate(req.body);
+      console.log('Successfully processed update');
+      res.status(200).send('OK');
+    } catch (error) {
+      console.error('Error processing update:', error);
+      res.status(500).send('Internal Server Error');
+    }
   } else {
+    console.log('Received non-POST request');
     res.status(200).send('Telegram Bot is running!');
   }
 };
