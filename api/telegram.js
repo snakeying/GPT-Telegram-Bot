fe54a509f01fb1bd9f607ced486762bbdb0c621b
snakeying <65364563+snakeying@users.noreply.github.com>
2024-09-12 03:21:17 +0800

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,15 +1,16 @@
-const { bot } = require('../src/bot');
+const { bot, handleMessage } = require('../src/bot');
 
 module.exports = async (req, res) => {
   try {
     if (req.method === 'POST') {
-      await bot.handleUpdate(req.body);
-      res.status(200).send('OK');
-    } else {
-      res.status(200).send('Hello, this is your Telegram bot!');
+      const { message } = req.body;
+      if (message) {
+        await handleMessage(message);
+      }
     }
+    res.status(200).send('OK');
   } catch (error) {
-    console.error('Error in webhook handler:', error);
+    console.error("Error in webhook handler:", error);
     res.status(500).send('Internal Server Error');
   }
 };
