d84b7f59c7dcc08e411f8af9ec14e4ff1a5d9315
snakeying <65364563+snakeying@users.noreply.github.com>
2024-09-16 17:52:51 +0800

    Update localization.js

--- src/localization.js
+++ src/localization.js
@@ -15,7 +15,7 @@ supportedLanguages.forEach(lang => {
 
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  token: process.env.UPSTASH_REST_TOKEN,
 });
 
 async function getUserLanguage(userId) {
